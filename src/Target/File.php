40bbe3eb3996637c20157177c4f54a0762ad9e5a
Daniil <sholokhovdaniil@yandex.ru>
2025-03-12 08:38:39 +0700

    Импорт файлов

--- src/Target/File.php
+++ src/Target/File.php
@@ -3,31 +3,53 @@
 namespace Sholokhov\Exchange\Target;
 
 use CFile;
-
 use Iterator;
 
 use Sholokhov\Exchange\Application;
 use Sholokhov\Exchange\Messages\Result;
 use Sholokhov\Exchange\Messages\Type\DataResult;
 
+use Bitrix\Main\ArgumentException;
+use Bitrix\Main\FileTable;
+use Bitrix\Main\ObjectPropertyException;
+use Bitrix\Main\SystemException;
+
 use Psr\Log\LoggerAwareTrait;
 use Psr\Container\NotFoundExceptionInterface;
 use Psr\Container\ContainerExceptionInterface;
 
 /**
- * Сохранение файла
+ * Импорт файла
  */
 class File extends Application
 {
     use LoggerAwareTrait;
 
     /**
+     * Обработка конфигураций обмена
+     *
+     * @param array $options
+     * @return array
+     */
+    protected function normalizeOptions(array $options): array
+    {
+        if (empty($options['module_id']) || !is_string($options['module_id'])) {
+            $options['module_id'] = 'iblock';
+        }
+
+        return parent::normalizeOptions($options);
+    }
+
+    /**
      * Выполнить обмен данными
      *
      * @param Iterator $source
      * @return Result
+     * @throws ArgumentException
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
+     * @throws ObjectPropertyException
+     * @throws SystemException
      */
     public function execute(Iterator $source): Result
     {
@@ -36,7 +58,10 @@ class File extends Application
 
         foreach ($source as $path) {
             if (is_string($path) && strlen($path)) {
-                $values[] = $this->getOptions()->get('use_cache') ? $this->saveByCache($path) : $this->save($path);
+                $id = $this->getFile($path) ?: $this->save($path);
+                if ($id) {
+                    $values[] = $id;
+                }
             }
         }
 
@@ -46,22 +71,39 @@ class File extends Application
     }
 
     /**
-     * Сохранить изображение с использованием кэша обмена
+     * Получение существующего файла
      *
      * @param string $path
      * @return int
+     * @throws ArgumentException
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
+     * @throws ObjectPropertyException
+     * @throws SystemException
      */
-    private function saveByCache(string $path): int
+    protected function getFile(string $path): int
     {
-        if ($this->cache->has($path)) {
-            return $this->cache->get($path);
-        } else {
-            $id = $this->save($path);
-            $this->cache->setField($path, $id);
-            return $id;
+        $fileId = 0;
+        $externalID = $this->getExternalId($path);
+        if ($this->cache->has($externalID)) {
+            $fileId = $this->cache->get($externalID);
+        } elseif ($file = FileTable::getRow(['filter' => ['EXTERNAL_ID' => $externalID], 'select' => ['ID']])) {
+            $fileId = (int)$file['ID'];
+            $this->cache->setField($externalID, $fileId);
         }
+
+        return $fileId;
+    }
+
+    /**
+     * Получение внешнего идентификатора файла
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function getExternalId(string $path): string
+    {
+        return md5($path);
     }
 
     /**
@@ -80,27 +122,13 @@ class File extends Application
             return 0;
         }
 
+        $file['external_id'] = $this->getExternalId($path);
         $file['MODULE_ID'] = $this->getOptions()->get('MODULE_ID');
 
-        return (int)CFile::SaveFile($file, $file['MODULE_ID']);
-    }
-
-    /**
-     * Обработка конфигураций обмена
-     *
-     * @param array $options
-     * @return array
-     */
-    protected function normalizeOptions(array $options): array
-    {
-        if (empty($options['MODULE_ID']) || !is_string($options['MODULE_ID'])) {
-            $options['MODULE_ID'] = 'iblock';
+        if ($fileId = (int)CFile::SaveFile($file, $file['MODULE_ID'])) {
+            $this->cache->setField($path, $fileId);
         }
 
-        if (!isset($options['USE_CACHE']) || !is_bool($options['USE_CACHE'])) {
-            $options['USE_CACHE'] = true;
-        }
-
-        return parent::normalizeOptions($options);
+        return $fileId;
     }
 }
