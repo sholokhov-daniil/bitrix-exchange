4837a93b7c74201a92802067aa368b5e0e804af8
Daniil <sholokhovdaniil@yandex.ru>
2025-05-07 22:42:01 +0700

    work(prepare): Создан преобразователь пути до файла в формат, который воспринимается превью и детальной картинкой элемента

---
+++ src/Prepares/IBlock/Element/File.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sholokhov\BitrixExchange\Prepares\IBlock\Element;
+
+use CFile;
+
+use Sholokhov\BitrixExchange\Prepares\AbstractPrepare;
+
+use Sholokhov\Exchange\Fields\FieldInterface;
+
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerAwareInterface;
+
+/**
+ * Преобразование пути до файла в массив формата $_FILES {@see CFile::MakeFileArray()}
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+class File extends AbstractPrepare implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * Проверка поддержки свойства
+     *
+     * @param mixed $value Значение, которое необходимо преобразовать
+     * @param FieldInterface $field Свойство, которое преобразовывается
+     * @return bool
+     *
+     * @version 1.0.0
+     * @since 1.0.0
+     */
+    public function supported(mixed $value, FieldInterface $field): bool
+    {
+        return in_array($field->getCode(), ['PREVIEW_PICTURE', 'DETAIL_PICTURE']);
+    }
+
+    /**
+     * Логика преобразования значения
+     *
+     * @param mixed $value
+     * @param FieldInterface $field
+     * @return mixed
+     *
+     * @version 1.0.0
+     * @since 1.0.0
+     */
+    protected function logic(mixed $value, FieldInterface $field): array
+    {
+        return CFile::MakeFileArray($value);
+    }
+}
