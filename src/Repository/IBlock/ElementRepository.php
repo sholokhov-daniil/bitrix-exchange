decf8370013f95468032fb5d47653bd99b84e8ca
Daniil <sholokhovdaniil@yandex.ru>
2025-05-14 21:04:13 +0700

    work(iblock): Создание хранилища элементов ИБ

---
+++ src/Repository/IBlock/ElementRepository.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Sholokhov\BitrixExchange\Repository\IBlock;
+
+use _CIBElement;
+use Bitrix\Main\Loader;
+use Bitrix\Main\LoaderException;
+use CIBlockElement;
+use Psr\Container\ContainerInterface;
+use Sholokhov\Exchange\Repository\Types\Memory;
+
+class ElementRepository implements ContainerInterface
+{
+    /**
+     * Хранилище данных
+     *
+     * @var Memory
+     *
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    private static Memory $storage;
+
+    /**
+     * @param int $iBlockID Информационный блок с которого необходимо получать данные
+     *
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    public function __construct(private readonly int $iBlockID)
+    {
+    }
+
+    public function get(string $id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has(string $id): bool
+    {
+        // TODO: Implement has() method.
+    }
+
+    /**
+     * Поиск элемента
+     *
+     * @param string $xmlId
+     * @return _CIBElement|null
+     * @throws LoaderException
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    private function query(string $xmlId): ?_CIBElement
+    {
+        if (!Loader::includeModule('iblock')) {
+            throw new LoaderException('Module "iblock" not installed');
+        }
+
+        return CIBlockElement::GetList([], ['XML_ID' => $xmlId])->GetNextElement() ?: null;
+    }
+
+    /**
+     * Получение хранилища данных свойств инфоблока
+     *
+     * @return Memory
+     *
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    private function getStorage(): Memory
+    {
+        return self::$storage ??= new Memory;
+    }
+}
--- src/Target/IBlock/LinkElement.php
+++ src/Target/IBlock/LinkElement.php
@@ -4,6 +4,9 @@ namespace Sholokhov\BitrixExchange\Target\IBlock;
 
 use Exception;
 
+use Psr\Log\LoggerAwareTrait;
+use Sholokhov\Exchange\Application;
+use Sholokhov\Exchange\ExchangeInterface;
 use Sholokhov\Exchange\Messages\ResultInterface;
 use Sholokhov\Exchange\Messages\Type\DataResult;
 
@@ -16,42 +19,44 @@ use Sholokhov\Exchange\Messages\Type\DataResult;
  * @version 1.0.0
  * @since 1.0.0
  */
-class LinkElement extends Element
+class LinkElement implements ExchangeInterface
 {
-    /**
-     * Обновление элемента
-     *
-     * @param array $item
-     * @return ResultInterface
-     * @throws Exception
-     *
-     * @version 1.0.0
-     * @since 1.0.0
-     */
-    protected function update(array $item): ResultInterface
-    {
-        $result = new DataResult;
-        $keyField = $this->getPrimaryField();
+    use LoggerAwareTrait;
 
-        $itemID = $this->cache->get($item[$keyField->getCode()]);
-
-        if (!$itemID) {
-            return $this->add($item);
-        }
-
-        return $result->setData((int)$itemID);
-    }
-
-    /**
-     * Деактивация элементов, которые не пришли в импорте
-     *
-     * @return void
-     *
-     * @version 1.0.0
-     * @since 1.0.0
-     */
-    protected function deactivate(): void
-    {
-        return;
-    }
+//    /**
+//     * Обновление элемента
+//     *
+//     * @param array $item
+//     * @return ResultInterface
+//     * @throws Exception
+//     *
+//     * @version 1.0.0
+//     * @since 1.0.0
+//     */
+//    protected function update(array $item): ResultInterface
+//    {
+//        $result = new DataResult;
+//        $keyField = $this->getPrimaryField();
+//
+//        $itemID = $this->cache->get($item[$keyField->getCode()]);
+//
+//        if (!$itemID) {
+//            return $this->add($item);
+//        }
+//
+//        return $result->setData((int)$itemID);
+//    }
+//
+//    /**
+//     * Деактивация элементов, которые не пришли в импорте
+//     *
+//     * @return void
+//     *
+//     * @version 1.0.0
+//     * @since 1.0.0
+//     */
+//    protected function deactivate(): void
+//    {
+//        return;
+//    }
 }
