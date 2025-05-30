fb639ca9f6a4a73e48605bf74a6e563336fbe7ba
Daniil <sholokhovdaniil@yandex.ru>
2025-05-31 00:15:30 +0700

    work(doc): Изменение шрифта в документации

--- build/doc/src/components/table/TableBlock.vue
+++ build/doc/src/components/table/TableBlock.vue
@@ -1,10 +1,18 @@
 <template>
-  <table class="table table-bordered">
-    <thead>
-      <slot name="head"></slot>
-    </thead>
-    <tbody>
-      <slot></slot>
-    </tbody>
-  </table>
+  <div class="table-container">
+    <table class="table table-bordered">
+      <thead>
+        <slot name="head"></slot>
+      </thead>
+      <tbody>
+        <slot></slot>
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<style scoped>
+.table-container {
+  overflow: auto;
+}
+</style>
