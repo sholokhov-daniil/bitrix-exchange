92ed73b30b87a691f26f25409f8efc28b6249ee1
Шолохов Даниил <sholokhovdaniil@yandex.ru>
2025-06-01 20:30:53 +0700

    Update version.php

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.0.3",
-    "VERSION_DATE" => "2025-06-01 14:25:00"
+    "VERSION" => "1.0.4",
+    "VERSION_DATE" => "2025-06-01 20:30:00"
 );
