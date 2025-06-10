e34a8c05ca964907ef0cd3afbbfdb13828823466
sholokhov-daniil <sholokhov.daniil@yandex.ru>
2025-06-10 22:10:45 +0700

    feature(events): Подписка на события

---
+++ events.php
@@ -0,0 +1,12 @@
+<?php
+
+use Bitrix\Main\EventManager;
+use Sholokhov\Exchange\Builder\ModuleMenu;
+
+$eventManager = EventManager::getInstance();
+
+$eventManager->addEventHandler(
+    'main',
+    'OnBuildGlobalMenu',
+    (new ModuleMenu())->make(...)
+);
--- include.php
+++ include.php
@@ -3,6 +3,7 @@
 use Sholokhov\Exchange\Bootstrap\Configuration;
 
 @require_once 'vendor/autoload.php';
+@require_once 'events.php';
 
 // Инициализация конфигураций
 $configBootstrap = new Configuration();
